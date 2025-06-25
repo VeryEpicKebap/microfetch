0237110e0157c551d9b4c196bff05689d9fe22d7
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-06-25 15:55:02 +0300

    updated mfetch_config.py to match with version 0.4

--- src/mfetch_config.py
+++ src/mfetch_config.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/python
-from microfetch import dcl,rel,ver,cpu,mod
-used=(dcl,rel,ver,cpu,mod)
+#!/usr/bin/python3
+from microfetch import Md as m
+used=(m.usr,m.dcl,m.rel,m.rel,m.ver,m.cpu,m.hos,m.mod,m.end)
 
