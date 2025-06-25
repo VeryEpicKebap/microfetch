50b30b257fdd784990dcfb73d7c15cf6680ac3f4
VeryEpicKebap <164046592+VeryEpicKebap@users.noreply.github.com>
2025-06-25 15:15:06 +0300

    Version 0.4

--- src/microfetch.py
+++ src/microfetch.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env/python
-# | microfetch 0.2
+#!/usr/bin/python3
+# | microfetch 0.4
 # | written by VeryEpicKebap - June 23rd 2025
 
+v="microfetch (version 0.3)"
 import os
 import argparse
+import subprocess
+import sys
+sys.path.insert(0,f"{os.getcwd}")
 from mfetch_config import *
-global used
+global used 
 RESET = "\033[0m"
 COLORS = {
     "Arch":     "\033[38;5;39m",     
@@ -30,7 +34,7 @@ def get_distro():
     except FileNotFoundError:
         pass
     if os.path.exists("/system/build.prop"):
-        return "Android (detected)"
+        return "Android"
     try:
         with open("/proc/version") as f:
             version = f.read().lower()
@@ -50,28 +54,49 @@ def dc(distro_name):
 dname=dc(get_distro())
 with open("/sys/devices/virtual/dmi/id/product_name") as f:
     model=f.read().strip()
+
 cpui=os.popen("grep -m1 -i 'model name' /proc/cpuinfo").read().strip().split(":", 1)[1]
-addons={
-     "001": f"System: {get_distro()}",
-     "002": f"System: {dname}",
-     "003": f"Kernel: {os.uname().release}",
-     "004": f"Version: {os.uname().version}",
-     "005": f"Host: {os.uname().version}",
-     "006": f"CPU:{cpui}",
-     "007": f"Model: {model}",
-}
-dnc = addons.get("001")
-dcl = addons.get("002")
-rel = addons.get("003")
-ver = addons.get("004")
-cpu = addons.get("006")
-mod = addons.get("007")    
+# revision 4: ascii art logos
+# logo-related parts of this program were hardcoded for use in Arch Linux only. future updates will bring logo support for every distro detectable by this program.,
+arch = [
+  r"      _    ",
+  r"     / \   ",
+  r"    /   \  ",
+  r"   /  .  \ ",
+  r"  /._| |_.\ ",
+]
+
+
 user=os.getlogin()
+# revision 3: simplified add-on system
+class Md:
+ usr = f"  ------ {user} ------"
+ end = f"  --------------------\n"
+ dnc = f"  System: {get_distro()}"
+ dcl = f"  System: {dname}"
+ rel = f"  Kernel: {os.uname().release}"
+ ver = f"  Version: {os.uname().version}"
+ hos = f"  Host: {os.uname().nodename}"
+ cpu = f"  CPU:{cpui}"
+ mod = f"  Model: {model}"
+   
 
 def main():
     global dname
     uname = os.uname()
-    print(f"\n------ {user} ------");outp='\n'.join(used);print(outp, "\n-------------------\n")
+    width = max(len(line) for line in arch)
+    for i in range(max(len(arch), len(used))): 
+     a = arch[i] if i < len(arch) else ''
+     b = used[i] if i < len(used) else ''
+     print(f"\033[38;5;39m{a.ljust(width)}{RESET}",b)
 
 if __name__ == "__main__":
-    main()
+    if "-ch" in sys.argv:
+     print(f"{v}\navailable display modules")
+     for name in vars(Md):
+      if not name.startswith("__"):
+       print(f"- {name}")
+    elif "-v" in sys.argv:
+     print(v)
+    else:
+     main()
